149c8ab7ab4c66639d4f5b17986581439c7d431b
Julian-Aguilar <106701606+Julian-Aguilar@users.noreply.github.com>
2022-06-01 14:03:37 -0500

    Add files via upload

---
+++ profecto final.js
@@ -0,0 +1,4 @@
+let comienso= "Bienvenido a esta página wed donde hablamos sobre el carnotaurus (aquí no hay ningún virus)"
+alert(comienso)
+let mensaje="Si lees este mensaje entonces eres hacker."
+console.log(mensaje)
